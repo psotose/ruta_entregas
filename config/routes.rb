118565f000a7bcdded2a8edec78149209e9e6660
patito <p.sotosellanes@hotmail.com>
2020-11-24 18:40:58 -0300

    bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'map_routes/index'
+  get 'map_routes/new'
+  get 'map_routes/create'
+  get 'map_routes/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
