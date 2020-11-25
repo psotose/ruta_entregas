104d837c6b58688a2cca7bac5c606f5ddb33ae6e
patito <p.sotosellanes@hotmail.com>
2020-11-25 10:48:10 -0300

    file key validations

--- app/controllers/map_routes_controller.rb
+++ app/controllers/map_routes_controller.rb
@@ -1,5 +1,6 @@
 class MapRoutesController < ApplicationController
   def index
+    @map_routes = MapRoute.all
   end
 
   def new
@@ -7,7 +8,16 @@ class MapRoutesController < ApplicationController
 
   def create
     uploaded_file = params[:archivo]
-    @stops = JSON.parse(uploaded_file.read)
+    @map_stops = JSON.parse(uploaded_file.read)
+    keys = ["nid", "base", "llegada", "salida", "carga", "destino"]
+    @map_stops.each do |stop|
+      stop[:status] = 'OK'
+      keys.each do |key|
+        if !stop.keys.include?(key)
+          stop[:status] = 'FAIL'
+        end  
+      end  
+    end
     
     
   end
