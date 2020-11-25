ca0c2b2931d774d6c99c4de148a077eb2a75f6a2
patito <p.sotosellanes@hotmail.com>
2020-11-24 22:21:06 -0300

    initial form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'map_routes/index'
-  get 'map_routes/new'
-  get 'map_routes/create'
+  get 'map_routes/new', to: 'map_routes#new', as: 'new'
+  post 'map_routes', to:'map_routes#create'
   get 'map_routes/show'
+
+  root 'map_routes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
