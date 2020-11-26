0f198cdae1a7f1ec2ec83a1754a5274331c0e522
patito <p.sotosellanes@hotmail.com>
2020-11-25 23:52:33 -0300

    map

--- app/controllers/map_stops_controller.rb
+++ app/controllers/map_stops_controller.rb
@@ -3,10 +3,24 @@ class MapStopsController < ApplicationController
 
   def show 
     @map_stops = MapStop.where(map_route: @map_route)
+
+    @markers = create_markers(@map_stops)
+
   end
 
   private
   def set_map_route
     @map_route = MapRoute.find(params[:id])
-  end 
+  end
+  
+  def create_markers(stops)
+    #&markers=color:blue%7Clabel:S%7C40.702147,-74.015794
+    markers = ''
+    stops.each do |stop|
+      markers += "&markers=color:blue%7Clabel:S%7C#{stop[:destino].gsub(';', ',')}"
+    end
+    markers
+  end
+
+
 end
