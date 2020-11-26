e568b99e111d0afc533b48db157886aaec8c7d12
patito <p.sotosellanes@hotmail.com>
2020-11-26 14:52:14 -0300

    error handling and read me

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
+App web que permite ingresar rutas y paradas mediante archivos json y visualizar la información.
+
 *Tecnologías utilizadas: Ruby 2.6.4, Rails 5.2.4 y base de datos PostgreSQL*
 
+## Consideraciones:
+
+- La ruta no se guarda si alguno de los campos de las paradas no corresponde con los definidos o si sus valores son nulos o vacíos.
+- El total de tiempo de cada ruta se calculo en base a las horas de llegada y salida contenidas en el archivo.
+- Se utilizo la versión estática de GoogleMaps. Las paradas ingresadas deben tener coordenadas contenidas en la Región Metropolitana de Santiago de Chile.
+- La app no es responsive.
+- No se incluyo validaciones para el contenido de los campos del archivo (coordenadas, horas, etc), por lo que los archivos deben respetar el formato modelo.
+```
+  [{
+	  "nid": "zoo",
+	  "base": "-33.459522;-70.631662",
+	  "llegada": "10:35",
+	  "salida": "10:50",
+	  "carga": 20,
+	  "destino": "-33.416222;-70.594666"
+  }]
+```  
+- La app se encuentra desplegada en la siguiente ruta de [Heroku](https://ruta-entregas.herokuapp.com/) 
+
 ## Pasos para levantar el proyecto
-
--clonar el repo
--rails db:create
--rails db:migrate
--rails s
-
+```
+git clone https://github.com/psotose/ruta_entregas.git
+rails db:create
+rails db:migrate
+rails s
+```
--- app/controllers/map_routes_controller.rb
+++ app/controllers/map_routes_controller.rb
@@ -11,56 +11,62 @@ class MapRoutesController < ApplicationController
 
   def create
     uploaded_file = params[:archivo]
-    @map_stops = JSON.parse(uploaded_file.read)
     keys_base = ["nid", "base", "llegada", "salida", "carga", "destino"]
     @error = ''
     valid = true
-    # Valida que el file tiene todos los campos necesarios
-    @map_stops.each do |stop|
-      stop[:status] = 'OK'
-      keys_base.each do |key|
-        if !stop.keys.include?(key)
-          stop[:status] = 'FAIL'
-          valid = false
-        end
-      end
-      # valida que los valores no sean ni nil ni vacíos
-      stop.values.each do |value|
-        if value == nil || value == ""
-          stop[:status] = 'FAIL'
-          valid = false
-        end  
-      end  
-    end
-
-    if valid
-      route = {}
-      stops_sorted = @map_stops.sort_by { |map| map["llegada"] }
-      route[:base] = stops_sorted.first["base"]
-      route[:start_time] = stops_sorted.first["llegada"]
-      route[:delivery_time] = stops_sorted.last["salida"]
-      route[:cargo] = stops_sorted.inject(0) {|sum, n| sum + n["carga"].to_i}
-      route[:route_time] = route_total_time(route[:start_time],route[:delivery_time])
-      route[:route_name] = params[:route_name]
-      route[:file_name] = uploaded_file.original_filename
-      route[:date] = params[:date]
-
-      @map_route = MapRoute.new(route)
+    # Manejo de errores en caso de inconsistencia principalmente en el archivo
+    begin
+      @map_stops = JSON.parse(uploaded_file.read)
+      # Valida que el file tiene todos los campos necesarios
       @map_stops.each do |stop|
-        @map_route.map_stops.new(stop.slice("nid", "base", "llegada", "salida", "carga", "destino"))
-      end  
-      respond_to do |format|      
-        if @map_route.save
-          format.html { render "create", notice: 'La ruta ha sido generada éxitosamente.' }      
-        else
-          @error = 'No ha sido posible generar la ruta.'
-          format.html { render "create"}      
+        stop[:status] = 'OK'
+        keys_base.each do |key|
+          if !stop.keys.include?(key)
+            stop[:status] = 'FAIL'
+            valid = false
+          end
         end
+        # valida que los valores no sean ni nil ni vacíos
+        stop.values.each do |value|
+          if value == nil || value == ""
+            stop[:status] = 'FAIL'
+            valid = false
+          end  
+        end  
       end
-    else
-      @error = 'Hay valores en el archivo que no pudieron ser cargados. Revise la lista y arregle los valores.'
+  
+      if valid
+        route = {}
+        stops_sorted = @map_stops.sort_by { |map| map["llegada"] }
+        route[:base] = stops_sorted.first["base"]
+        route[:start_time] = stops_sorted.first["llegada"]
+        route[:delivery_time] = stops_sorted.last["salida"]
+        route[:cargo] = stops_sorted.inject(0) {|sum, n| sum + n["carga"].to_i}
+        route[:route_time] = route_total_time(route[:start_time],route[:delivery_time])
+        route[:route_name] = params[:route_name]
+        route[:file_name] = uploaded_file.original_filename
+        route[:date] = params[:date]
+  
+        @map_route = MapRoute.new(route)
+        @map_stops.each do |stop|
+          @map_route.map_stops.new(stop.slice("nid", "base", "llegada", "salida", "carga", "destino"))
+        end  
+        respond_to do |format|      
+          if @map_route.save
+            format.html { render "create", notice: 'La ruta ha sido generada éxitosamente.' }      
+          else
+            @error = 'No ha sido posible generar la ruta.'
+            format.html { render "create"}      
+          end
+        end
+      else
+        @error = 'Hay valores en el archivo que no pudieron ser cargados. Revise la lista y arregle los valores.'
+      end    
+    rescue StandardError => e
+      puts e
+      @map_stops = []
+      @error = 'Ocurrio un error inesperado, no es posible procesar su solicitud, vuelva a intentarlo'
     end
-
   end
 
   def show
